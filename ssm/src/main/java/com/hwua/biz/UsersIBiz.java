ac7e1e4c9147ba9c0b8ca695baaa0e3a9648d6c4
cqmaple <404069322@qq.com>
2018-11-23 14:42:52 +0800

    项目架构

---
+++ ssm/src/main/java/com/hwua/biz/UsersIBiz.java
@@ -0,0 +1,26 @@
+package com.hwua.biz;
+
+import java.util.List;
+
+import com.hwua.entity.Users;
+
+/**
+ * 
+ * @ClassName:  UsersIBiz   
+ * @Description:TODO(这里用一句话描述这个类的作用)   
+ * @author: maple
+ * @date:   2018年11月23日 上午10:42:55   
+ *     
+ * @Copyright: 2018 www.hwua.com Inc. All rights reserved. 
+ * 注意：本内容仅限于海文科信息技术股份有限公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public interface UsersIBiz {
+	public List<Users> getAllUsers();
+	
+	public boolean updateUsers();
+	
+	public boolean addUsers();
+	11111111
+	222222222
+	
+}
